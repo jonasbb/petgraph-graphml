cb03ba3c82ce88e54f636a76ddfeb0d589ba6c81
Jonas Bushart <jonas@bushart.org>
2018-09-19 22:42:26 +0200

    Reformat files

--- src/lib.rs
+++ src/lib.rs
@@ -75,8 +75,14 @@
 //!
 
 #![deny(
-    missing_debug_implementations, missing_copy_implementations, missing_docs, trivial_casts,
-    trivial_numeric_casts, unused_extern_crates, unused_import_braces, unused_qualifications,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    missing_docs,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
     variant_size_differences
 )]
 
